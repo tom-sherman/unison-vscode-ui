8f9bd2b04abe6410a4189e579dae5ae08e31474f
Tom Sherman <the.tomsherman@gmail.com>
2021-09-29 18:20:35 +0100

    Save base url in settings

--- src/extension.ts
+++ src/extension.ts
@@ -4,10 +4,30 @@ import * as vscode from 'vscode';
 import { createApiClient } from './api';
 import { CodebaseProvider } from './tree-view';
 
+const BASE_URL_CONFIG_NAME = 'unison-ui.apiBaseUrl';
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	let codebaseProvider: null | CodebaseProvider = null;
+	function setupCodebaseProvider(unisonUrl: string) {
+		const apiClient = createApiClient(unisonUrl);
+
+		codebaseProvider = new CodebaseProvider(apiClient);
+
+		const treeView = vscode.window.createTreeView('codebase', {
+			treeDataProvider: codebaseProvider,
+		});
+
+		context.subscriptions.push(treeView);
+	}
+
+	const configuration = vscode.workspace.getConfiguration();
+	const savedUnisonUrl = configuration.get<string>(BASE_URL_CONFIG_NAME);
+
+	if (savedUnisonUrl) {
+		setupCodebaseProvider(savedUnisonUrl);
+	}
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand(
@@ -40,15 +60,8 @@ export function activate(context: vscode.ExtensionContext) {
 			return;
 		}
 
-		const apiClient = createApiClient(unisonUrl);
-
-		codebaseProvider = new CodebaseProvider(apiClient);
-
-		const treeView = vscode.window.createTreeView('codebase', {
-			treeDataProvider: codebaseProvider,
-		});
-
-		context.subscriptions.push(treeView);
+		setupCodebaseProvider(unisonUrl);
+		await configuration.update(BASE_URL_CONFIG_NAME, unisonUrl);
 	}
 }
 
